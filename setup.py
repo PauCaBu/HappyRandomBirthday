a4a5f0a3b91bfbfdf364871ea2ee9063f8ac808e
Paula Cáceres Burgos <43546843+PauCaBu@users.noreply.github.com>
2022-03-29 13:07:34 -0300

    actualizando a version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HappyRandomBirthday',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     author="Pola y Raza",
     description="Virtual Random Birthday cards",
